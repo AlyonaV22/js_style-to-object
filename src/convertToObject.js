f35ee8fd9d5fbeee5cffa309151e942392f8e900
Alyona Verbanova <alyona.verbanova22@gmail.com>
2024-11-04 17:57:04 +0100

    create object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const arrayString = sourceString.split(';');
+
+  for (let i = 0; i < arrayString.length; i++) {
+    const item = arrayString[i].trim().split(':');
+
+    if (item.length === 2) {
+      const key = item[0].trim();
+      const value = item[1].trim();
+
+      result[key] = value;
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
